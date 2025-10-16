aae762f9e64008e1b50607f7205d0933a88484e6
rahul8341 <rahul.pentester8341@gmail.com>
2025-10-16 11:50:38 +0530

    Create TestSuite1.java

---
+++ src/test/java/sampletest/TestSuite1.java
@@ -0,0 +1,22 @@
+package sampletest;
+
+//JUnit 5 annotations
+import org.junit.jupiter.api.Test;
+
+public class TestSuite1 {
+
+    @Test
+    public void test1() {
+
+        System.out.println("test1 passed.");
+
+    }
+
+   @Test
+    public void test2() {
+
+        System.out.println("test2 passed.");
+
+    }
+
+}
